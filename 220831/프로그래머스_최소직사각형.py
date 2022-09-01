1325f1a0f281155a1e26068f46e94fd8e0ec3bc6
forwardyoung <chahwayoung214@gmail.com>
2022-09-01 09:32:21 +0900

    220831

--- 220831/프로그래머스_최소직사각형.py
+++ 220831/프로그래머스_최소직사각형.py
@@ -1,2 +1,18 @@
 def solution(sizes):
-    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
+    answer = 0
+
+    sizes = [sorted(size, reverse=True) for size in sizes]
+
+    width = [size[0] for size in sizes]
+    height = [size[1] for size in sizes]
+
+    w, h = max(width), max(height)
+
+    answer = w*h
+
+    return answer
+
+# 간단한 풀이
+# def solution(sizes):
+#     return max(max(x) for x in sizes) * max(min(x) for x in sizes)
+
