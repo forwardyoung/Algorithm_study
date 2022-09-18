4dea40173b9bf9304c5b2b0fb4cd632898db2c8f
forwardyoung <chahwayoung214@gmail.com>
2022-09-19 00:03:09 +0900

    220918

--- 220918/SWEA_1970.py
+++ 220918/SWEA_1970.py
@@ -6,6 +6,7 @@ for tc in range(1, T+1):
 
     for m in money_list:
         cnt.append(price // m)  # 정수 몫(//)으로 price가 32850일 경우, 만원은 3개 필요함을 구한다.
-        price %= m  # 금액을 다시 설정할 때 m으로 나눈 나머지가 아닌 마이너스로 해도 된다.
+        price %= m  # price는 2850 850 350 50 0 순으로 설정된다.
+        # 금액을 다시 설정할 때 m으로 나눈 나머지가 아닌 마이너스로 해도 된다.
     print(f'#{tc}')
     print(*cnt)
