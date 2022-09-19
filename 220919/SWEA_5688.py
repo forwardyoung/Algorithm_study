d6f4ee7a2485e84b628a0399d00bc550842661e8
forwardyoung <chahwayoung214@gmail.com>
2022-09-19 23:17:39 +0900

    220919

--- 220919/SWEA_5688.py
+++ 220919/SWEA_5688.py
@@ -11,7 +11,7 @@ for tc in range(1, T+1):
 
 # 풀이 2
 # T = int(input())
-	
+
 # for tc in range(1, T+1):
 #     N = int(input())
 #     X = round(N**(1/3))
@@ -20,3 +20,24 @@ for tc in range(1, T+1):
 #         result = -1
 
 #     print(f'#{tc} {result}')
+
+# 풀이 3
+# T = int(input())
+
+# for tc in range(1, T + 1):
+
+#     N = int(input())
+#     s, e = 1, N
+#     ans = -1
+#     while s <= e:
+
+#         m = (s + e) // 2
+#         if N == m ** 3:
+#             ans = m
+#             break
+#         elif N < m ** 3:
+#             e = m - 1
+#         else:
+#             s = m + 1
+
+#     print(f'#{tc} {ans}')
