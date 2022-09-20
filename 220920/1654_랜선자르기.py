bd7e46beb2b4d52d95f544827d6096c45386553f
forwardyoung <chahwayoung214@gmail.com>
2022-09-21 00:22:58 +0900

    220920

--- 220920/1654_랜선자르기.py
+++ 220920/1654_랜선자르기.py
@@ -7,8 +7,9 @@ length = []  # K개의 각 랜선의 길이를 담을 리스트
 for i in range(K):
     length.append(int(input()))
 
-start = 1
-end = max(length)
+# 랜선 길이는 1부터 가장 긴 랜선의 길이까지
+start = 1  
+end = max(length) # 가장 긴 랜선의 길이 => print할 것
 
 while (start <= end):
     mid = (start + end) // 2
