1fb50089809d812574604331324064bca19d3279
forwardyoung <chahwayoung214@gmail.com>
2022-09-27 23:54:12 +0900

    220927

---
+++ 220927/SWEA_1285_unsolved.py
@@ -0,0 +1,9 @@
+# 알고보니 C++로 풀어야 하는 문제
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     stone = list(map(int, input().split())) # N명의 사람이 던진 돌의 위치
+#     num = []
+#     for i in stone:
+#         num.append(abs(i))
+        
