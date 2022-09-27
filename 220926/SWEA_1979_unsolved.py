c1333878ce7212d4a9ac394cc39ec386af8a96b0
forwardyoung <chahwayoung214@gmail.com>
2022-09-28 00:06:47 +0900

    220926_File_remove

--- 220926/SWEA_1979_unsolved.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for tc in range(1, T+1):
-    N, K = map(int, input().split())
-    for _ in range(N):
-        shape = list(map(int, input().split()))
-    # shape = [list(map(int, input().split())) for _ in range(N)] list comprehension으로 작성 가능
-    
