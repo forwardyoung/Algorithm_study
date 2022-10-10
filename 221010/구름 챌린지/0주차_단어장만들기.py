8e4f6725f5249362549861d4fa5581d0768b4af1
forwardyoung <chahwayoung214@gmail.com>
2022-10-10 14:44:12 +0900

    221010 edit_구름0주차

--- 221010/구름 챌린지/0주차_단어장만들기.py
+++ 221010/구름 챌린지/0주차_단어장만들기.py
@@ -7,3 +7,10 @@ list_.sort()
 list_.sort(key = lambda x : len(x)) # 이 코드가 없다면 list_ = ['a', 'aa', 'aaa', 'aaaa', 'b', 'bb', 'bbb']
 # print(list_) ['a', 'b', 'aa', 'bb', 'aaa', 'bbb', 'aaaa']
 print(list_[k-1])
+
+# 해설 코드
+# word = list()
+# n, k = map(int, input().split())
+# for i in range(n):
+#     word.append(input().rstrip()) # rstrip()을 사용해 입력 받은 단어들을 공백 없이 리스트에 담는다.
+# print(word)
