a76555faef4369e3e293f7e29337c21ac36d830d
forwardyoung <chahwayoung214@gmail.com>
2022-10-13 14:12:18 +0900

    221013 구름_1주차_최장맨해튼거리

--- 221013/구름 챌린지/1주차_최장맨해튼거리.py
+++ 221013/구름 챌린지/1주차_최장맨해튼거리.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline # readline : 한 줄 씩 읽는다. readlines : 한 줄 한 줄이 각각 리스트의 원소로 들어간다.   
-num = list(map(int, input().split()))
+
+num = list(map(int, input().split())) # 4개의 정수를 입력받는다.
 num.sort()
 # print(num) [-10, -3, -1, 5]
 
