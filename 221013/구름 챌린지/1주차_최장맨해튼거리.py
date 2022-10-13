2c5bb89b2bb7815098761307e32fee0cdeb6cc26
forwardyoung <chahwayoung214@gmail.com>
2022-10-13 14:07:04 +0900

    221013 구름_1주차_UPDATE REAME

---
+++ 221013/구름 챌린지/1주차_최장맨해튼거리.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline # readline : 한 줄 씩 읽는다. readlines : 한 줄 한 줄이 각각 리스트의 원소로 들어간다.   
+num = list(map(int, input().split()))
+num.sort()
+# print(num) [-10, -3, -1, 5]
+
+x = abs(num[0] - num[2])
+y = abs(num[1] - num[3])
+# print(x, y) 9 8
+print(x+y)
+
+# print(num[3] +  num[2] - num[1] - num[0]) sort() 정렬 후 num에서 다음과 같이 계산해도 된다. 4-(-13) = 17
