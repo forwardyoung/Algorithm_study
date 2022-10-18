d4532dd5710313944770a811f077a73fb41ffc5d
forwardyoung <chahwayoung214@gmail.com>
2022-10-18 23:27:39 +0900

    221018 ADD 11729_하노이탑

---
+++ 221018/11729_하노이탑.py
@@ -0,0 +1,13 @@
+def hanoi(num, a, b, c):
+    if num == 1:
+        move.append([a, c])
+    else:
+        hanoi(num-1, a, c, b)
+        move.append([a, c])
+        hanoi(num-1, b, a, c)
+
+move = [] # 이동 경로를 담을 list
+hanoi(int(input()), 1, 2, 3) # 재귀함수
+
+print(len(move)) # 이동 횟수
+print("\n".join([' '.join(str(i) for i in row) for row in move])) # 이동 경로 출력
