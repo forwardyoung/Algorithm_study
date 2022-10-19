53c7b8c263bc4693ad72c3c051f85b07f349231b
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2022-10-19 15:34:02 +0900

    Update 11729_하노이탑.py

--- 221018/11729_하노이탑.py
+++ 221018/11729_하노이탑.py
@@ -7,7 +7,8 @@ def hanoi(num, a, b, c):
         hanoi(num-1, b, a, c)
 
 move = [] # 이동 경로를 담을 list
-hanoi(int(input()), 1, 2, 3) # 재귀함수
+num = int(input())
+hanoi(num, 1, 2, 3) # 재귀함수
 
 print(len(move)) # 이동 횟수
 print("\n".join([' '.join(str(i) for i in row) for row in move])) # 이동 경로 출력
