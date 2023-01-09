b57ba249154b98fc06d7a772c6124c0516cceff7
forwardyoung <chahwayoung214@gmail.com>
2023-01-10 00:01:13 +0900

    ADD boj_10172.java

---
+++ java/boj_10172.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+// "출력을 위해서는 \"와 같이, '출력을 위해서는 \'와 같이 입력해야 함
