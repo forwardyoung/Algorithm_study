862ffdab5d2cce4b783629705374fef0f428bcd3
forwardyoung <chahwayoung214@gmail.com>
2023-01-09 23:59:26 +0900

    ADD boj_10171

---
+++ java/boj_10171.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");                  
+    }
+}
+
+// 자바 String 에서 \(역슬래시) 표기 : \\  
