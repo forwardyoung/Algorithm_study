995282702263b49e1141cad33e246f630791f437
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-02-05 14:51:16 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,7 +1,7 @@
 def solution(n):
-    square_root = n**(1/2)
+    square_root = n**(1/2) # n의 1/2거듭제곱은 곧 n의 제곱근
     
-    if int(square_root) == square_root:
-        return (square_root + 1)**2
-    else:
-        return -1
+    if int(square_root) == square_root: # n의 제곱근이 정수인지 판별
+        return (square_root + 1)**2 # 정수가 맞다면 1을 더한 후 제곱
+    else: # n의 제곱근이 이를 정수화한 값과 다르다면 
+        return -1 
