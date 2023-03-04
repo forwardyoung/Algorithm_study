0bd2c42d04a38238c0a98acf800d5c139f5d8252
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-03-04 13:48:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 87.81 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,14 +1,14 @@
 def solution(s):
     stack = []
     for letter in s:
-        if not stack:
+        if not stack: # if letter not in stack과 같은 의미
             stack.append(letter)
         else:
             if stack[-1] == letter:
                 stack.pop()
             else:
                 stack.append(letter)
-                
+
     if len(stack) == 0:
         answer = 1
     else:
