f98ce360e30b312be0c8832274b0478d7ad64305
forwardyoung <chahwayoung214@gmail.com>
2023-04-07 17:07:35 +0900

    UPDATE 프로그래머스_네트워크.py

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -5,10 +5,10 @@ def solution(n, computers):
             if computers[i][j] and not visited[j]:
                 DFS(j)
     answer = 0
-    visited = [0 for i in range(len(computers))]
+    visited = [0 for i in range(len(computers))] # 컴퓨터의 개수와 동일한 크기의 방문 리스트 생성
     for i in range(n):
-        if not visited[i]:
+        if not visited[i]: # 방문하지 않은 컴퓨터라면
             DFS(i)
-            answer += 1
+            answer += 1 # 네트워크 개수에 1을 더한다.
         
     return answer
