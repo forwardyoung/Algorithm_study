1c11b8fd14a5f4927b89718cb142c60601c662d0
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-04-13 15:14:11 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -5,6 +5,7 @@ def solution(begin, target, words):
         return 0
     q = deque ()
     q.append([begin, 0]) # 시작하는 단어, 깊이
+    V = [ 0 ] * (len(words))    # 방문 노드 여부 확인 리스트
     
     while q:
         word, cnt = q.popleft() # q에서 한 개를 뺀다
@@ -14,10 +15,13 @@ def solution(begin, target, words):
             
         for i in range(len(words)):
             temp_cnt = 0 
-            for j in range(len(word)):
-                    if word[j] != words[i][j]:
-                        temp_cnt += 1
-            if temp_cnt == 1:   # 만약 다른 글자 개수가 1개라면
-                    q.append([words[i], cnt+1])
+            if not V[i]:    # 만약 확인 안 한 단어라면
+                # 그 단어가 words 속 단어와 다를 때 한 자씩 비교해서 더하기
+                for j in range(len(word)):
+                        if word[j] != words[i][j]:
+                            temp_cnt += 1
+                if temp_cnt == 1:   # 만약 다른 글자 개수가 1개라면
+                        q.append([words[i], cnt+1])
+                        V[i] = 1
     return 0 # target값이 있으나 변환될 수 없다면
             
