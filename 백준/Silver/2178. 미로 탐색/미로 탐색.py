6fd3c41b8c6222cf24dbf3a1d04c0d7c7a85f923
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-04-16 00:47:31 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -27,3 +27,38 @@ while queue:
             if visited[nx][ny] == 0 and matrix[nx][ny] == '1':
                 visited[nx][ny] = visited[x][y] + 1
                 queue.append((nx,ny))
+
+# 다른 풀이
+# from collections import deque
+
+# 상하좌우 탐색
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+# N, M = map(int, input().split())
+# A = [[0]*M for _ in range(N)] # 데이터 저장 행렬
+# visited = [[False]*M for _ in range(N)] # 방문 기록 저장 리스트
+
+# for i in range(N):
+#     numbers = list(input())
+#     for j in range(M):
+#         A[i][j] = int(numbers[j])
+        
+# def BFS(i, j):
+#     queue = deque()
+#     queue.append((i, j)) # 큐에 시작 노드 삽입
+#     visited[i][j] = True # 현재 노드 방문 기록
+    
+#     while queue:
+#         now = queue.popleft() # 큐에서 노드 데이터 가져오기
+#         # 상하좌우 탐색
+#         for k in range(4):
+#             x = now[0] + dx[k]
+#             y = now[1] + dy[k]
+#             if x >= 0 and y >= 0 and x < N and y < M: # 좌표 유효성 검사
+#                 if A[x][y] != 0 and not visited[x][y]: # 해당 데이터의 값이 0이 아니고 방문하지 않은 경우 (1이어야 이동 가능) 
+#                     visited[x][y] = True # 방문 기록
+#                     A[x][y] = A[now[0]][now[1]] + 1 # A 리스트에 depth를 현재 노드의 depth + 1로 업데이트
+#                     queue.append((x, y)) # 큐에 데이터 삽입
+ 
+# BFS(0, 0)
+# print(A[N-1][M-1])
