39b724e556c736520644bf07fe6433e6bb42548c
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-04-22 16:07:56 +0900

    Update 단속카메라.py

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,6 +1,7 @@
 def solution(routes):
     answer = 0
     routes.sort(key=lambda x: x[1]) # routes를 차량 진출 지점 기준으로 정렬한다.
+    # 이중 리스트 key에 lambda 인자 : 표현식 형태로 정렬
     camera = -30001 # 차량 진입, 진출 지점은 -30000보다는 커야 하므로 -30001부터 카메라 위치 찾는다.
     for route in routes:
         if route[0] > camera: # 카메라 위치가 진입 지점보다 뒤에 있으면(차량과 카메라가 만나지 않으면)
