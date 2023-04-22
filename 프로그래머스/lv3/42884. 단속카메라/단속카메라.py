8a131f7395dc8066ec44868203e34ee1596f766f
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-04-22 15:56:48 +0900

    Update 단속카메라.py

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -4,6 +4,6 @@ def solution(routes):
     camera = -30001 # 차량 진입, 진출 지점은 -30000보다는 커야 하므로 -30001부터 카메라 위치 찾는다.
     for route in routes:
         if route[0] > camera: # 카메라 위치가 진입 지점보다 뒤에 있으면(차량과 카메라가 만나지 않으면)
-            answer += 1 # 카메라 개수에 1을 더한다.
-            camera = route[1] # 카메라 위치를 진출 지점으로 갱신
+            answer += 1 # 카메라를 새로 설치하고 개수에 1을 더한다.
+            camera = route[1] # 카메라 위치를 최근 카메라 진출 지점으로 갱신
     return answer 
