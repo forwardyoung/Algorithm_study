8c75359f5825a42a84bd3b0736d7819a8c912ec0
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-04-29 12:39:31 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME FROM ANIMAL_INS a join ANIMAL_OUTS b 
+ON a.ANIMAL_ID = b.ANIMAL_ID
+WHERE a.SEX_UPON_INTAKE like 'Intact%' and b.SEX_UPON_OUTCOME not like 'Intact%'
+ORDER BY a.ANIMAL_ID;
