ad966d1873c37b606495514a82b7fe4fb6c2b69f
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-04-29 09:00:30 +0900

    [unrated] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/README.md
+++ 프로그래머스/unrated/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [unrated] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#qna) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.06 ms
+메모리: 10.3 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -1,64 +1,43 @@
 from collections import deque
 
-#상하좌우
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
 def solution(maps):
-    #시작 지점
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] == "S":
+    answer = 0
+    direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    N = len(maps) # 미로의 세로 길이
+    M = len(maps[0]) # 미로의 가로 길이
+    
+    # 출발 지점
+    for i in range(N):
+        for j in range(M):
+            if maps[i][j] == 'S':
                 sx, sy = i, j
-
-    visited = [[0 for _ in range(len(maps[0])) ] for _ in range(len(maps))]
-    visited[sx][sy] = 1 #시작 지점은 방문 처리
-
-    que = deque([(sx,sy,0)])
-    lx = 0 
-    ly = 0
-    cnt = 0
-    flag = False
-    while que: #레버까지 가는데 최단 시간
-        x,y,cnt = que.popleft()
-        if maps[x][y] == "L":
-            lx, ly = x, y
-            flag = True
-            break
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or ny <0 or nx >= len(maps) or ny >= len(maps[0]) :
-                continue
-            if visited[nx][ny] == 1 or maps[nx][ny] == 'X':
-                continue
-
-            que.append((nx,ny,cnt + 1))
-            visited[nx][ny] = 1
-
-    if flag == False :
+                
+    # 레버 찾기
+    def bfs(x, y, end):
+        q = deque()
+        q.append([x, y])
+        visited = [[-1] * M for _ in range(N)] # 방문 여부 리스트 생성
+        visited[x][y] = 0
+        while q:
+            x, y = q.popleft()
+            if maps[x][y] == end:
+                return [visited[x][y], x, y]
+            for dir in direction:
+                nx = x + dir[0]
+                ny = y + dir[1]
+                if 0 <= nx < N and 0 <= ny < M: # 좌표 유효성 검사
+                    if visited[nx][ny] == -1:
+                        if maps[nx][ny] != 'X': # 벽으로 막혀있지 않다면
+                            q.append([nx, ny])
+                            visited[nx][ny] = visited[x][y] + 1
+        return None
+    cnt = bfs(sx, sy, 'L')
+    if cnt == None: # 이동할 수 없어 cnt 값이 없다면
+        return -1 # -1 출력
+    answer += cnt[0] 
+    cnt = bfs(cnt[1], cnt[2], 'E')
+    if cnt == None:
         return -1
-    visited = [[0 for _ in range(len(maps[0])) ] for _ in range(len(maps))]
-    que2 = deque([(lx,ly,cnt)])
-    visited[sx][sy] = 0
-    while que2: #종료까지 가는데 최단 시간
-        x,y,cnt = que2.popleft()
-        if maps[x][y] == "E":
-            return cnt
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or ny <0 or nx >= len(maps) or ny >= len(maps[0]) :
-                continue
-            if visited[nx][ny] == 1 or maps[nx][ny] == 'X':
-                continue
-
-            que2.append((nx,ny,cnt + 1))
-            visited[nx][ny] = 1
-
-
-    return -1
+    answer += cnt[0]
+    return answer
+    
