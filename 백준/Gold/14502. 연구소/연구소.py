1b47fa3c496107d3c5e6cc02261d9b7d9a478b3f
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-05-09 21:28:16 +0900

    Update 연구소.py

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -33,20 +33,20 @@ def bfs():
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < N and 0 <= ny < M:
-                if matrix[nx][ny] == 0 and visit[nx][ny] == 0:
-                    visit[nx][ny] = 2
-                    start.append((nx, ny))
+                if matrix[nx][ny] == 0 and visit[nx][ny] == 0: # 지도에서 탐색한 좌표가 빈 칸(0)이고 방문하지 않았다면 => 바이러스가 퍼져나갈 수 있음
+                    visit[nx][ny] = 2 # 방문 체크 리스트 2로 바꾸고
+                    start.append((nx, ny)) # 큐에 해당 위치 삽입
     cnt = 0 # 바이러스가 퍼지지 않은 곳    
     for i in range(N):
         for j in range(M):
             if visit[i][j] == 0:
                 cnt += 1
-    return cnt-wall-3 # wall(벽의 개수)- 내가 세운 벽의 개수 3
+    return cnt-wall-3 # cnt - wall(벽의 개수) - 내가 세운 벽의 개수 3 => 벽일 경우에 바이러스가 퍼질 수 없음
 result = [] # 안전영역 크기 리스트
 for i in wall3:
     for j in i:
         matrix[j[0]][j[1]] = 1 # 조합으로 벽이 되었다면 좌표 값을 1로 바꾼다.
-    result.append(bfs())
+    result.append(bfs()) # bfs 실행 후 return 값을 result 리스트에 추가
     for j in i:
         matrix[j[0]][j[1]] = 0
 print(max(result)) # 안전 영역 크기의 최댓값
