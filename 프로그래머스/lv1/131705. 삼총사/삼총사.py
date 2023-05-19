9ed3e560d9c89964a3c9a56d637050b65ff1f9e8
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-05-19 14:50:35 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import combinations # 조합
+def solution(number):
+    answer = 0
+    sum_ = list(map(sum, combinations(number, 3)))
+    for i in sum_:
+      if i == 0:
+        answer += 1
+    return answer
