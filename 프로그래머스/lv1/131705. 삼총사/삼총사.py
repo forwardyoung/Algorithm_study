f0c08fb78da59a2d188effd3f83f3a4aad24ffb0
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2023-05-19 14:54:26 +0900

    Update 삼총사.py

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -6,3 +6,7 @@ def solution(number):
         if i == 0:
             answer += 1
     return answer
+
+# 간단하고 pythonic한 코드
+# def solution(number):
+#     return list(map(sum, combinations(number, 3))).count(0) # map으로 list를 sum함수로 처리 후, count 함수로 0인 요소의 개수를 센다
